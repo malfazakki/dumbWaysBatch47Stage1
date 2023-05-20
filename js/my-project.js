5cc3cd67d8dbd34a2ff3c6627a9674b1a4927a9a
malfazakki <malfazakki@gmail.com>
2023-05-20 08:21:09 +0700

    update my-project page

--- js/my-project.js
+++ js/my-project.js
@@ -1,57 +1,64 @@
-let dataBlog = [];
+let dataProject = [];
 
-function addBlog(event) {
+function addProject(event) {
   event.preventDefault();
 
   let title = document.getElementById("project-name").value;
-  let content = document.getElementById("description").value;
+  let startDate = document.getElementById("start-date").value;
+  let endDate = document.getElementById("end-date").value;
+  let description = document.getElementById("description").value;
   let image = document.getElementById("image-upload").files;
 
-  // untuk membuat url gambar, agar tampil
+  if (
+    title === "" ||
+    startDate === "" ||
+    endDate === "" ||
+    description === "" ||
+    image === ""
+  ) {
+    alert("Pastikan semua kolom formulir terisi!");
+  }
+
   image = URL.createObjectURL(image[0]);
   console.log(image);
 
-  let blog = {
+  let project = {
     title,
-    content,
+    startDate,
+    endDate,
+    description,
     image,
-    duration: "1 bulan",
-    author: "Malfazakki",
   };
 
-  dataBlog.push(blog);
-  console.log(dataBlog);
+  dataProject.push(project);
+  console.log(dataProject);
 
-  renderBlog();
+  renderProject();
 }
 
-function renderBlog() {
-  document.getElementById("description").innerHTML = "";
-
-  for (let index = 0; index < dataBlog.length; index++) {
+function renderProject() {
+  for (let index = 0; index < dataProject.length; index++) {
     document.getElementById("contents").innerHTML += `
-        <div class="blog-list-item">
-            <div class="blog-image">
-                <img src="${dataBlog[index].image}" alt="blog_img" />
-            </div>
-            <div class="blog-content">
-            <div class="btn-group">
-                <button class="btn-edit">Edit Post</button>
-                <button class="btn-delete">Delete Post</button>
-            </div>
-            <h1>
-                <a href="blog-detail.html" target="_blank"
-                >${dataBlog[index].title}</a
-                >
-            </h1>
-            <div class="detail-blog-content">
-                ${dataBlog[index].postAt} | ${dataBlog[index].author}
-            </div>
-            <p>
-                ${dataBlog[index].content}
-            </p>
-            </div>
+      <div class="card">
+        <img src="${dataProject[index].image}" alt="${dataProject[index].title}" />
+        <h3 class="judul-artikel">
+          <a href="project-detail.html" target="_blank">${dataProject[index].title}</a>
+        </h3>
+        <div class="durasi">
+          <p class="durasi">${dataProject[index].startDate}</p>
+          <p class="durasi">${dataProject[index].endDate}</p>
         </div>
+        <p class="deskripsi-artikel">
+          ${dataProject[index].description}
+        </p>
+        <div class="techimages">
+          ${nodeJs}
+        </div>
+        <div class="btn-artikel">
+          <button class="btn-edit">edit</button>
+          <button class="btn-delete">delete</button>
+        </div>
+      </div>
     `;
   }
 }
