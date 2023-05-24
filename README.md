18d2e62bb80f49e6c872f492e3a15dcbf4805822
malfazakki <malfazakki@gmail.com>
2023-05-24 17:17:03 +0700

    Create Responsive Navbar

--- README.md
+++
@@ -1,5 +0,0 @@
-# DumbWays.id Bootcamp Batch 47 Stage 1
-### Task Week 1 - Day 4
-
-Deployment: 
-
--- js/my-project.js
+++ js/my-project.js
@@ -36,17 +36,19 @@ function addProject(event) {
   image = URL.createObjectURL(image[0]);
   const selesai = moment(startDate);
   const mulai = moment(endDate);
-  const dayDuration = mulai.diff(selesai, "days");
-  const monthDuration = mulai.diff(selesai, "months");
+  const daysDuration = mulai.diff(selesai, "days");
+  const monthsDuration = mulai.diff(selesai, "months");
   const yearsDuration = mulai.diff(selesai, "years");
   let duration;
 
-  if (monthDuration > 0) {
-    duration = `duration: ${monthDuration} months`;
-  } else if (yearsDuration > 0) {
-    duration = `duration: ${yearsDuration} years`;
+  if (yearsDuration > 0) {
+    duration = `duration: ${yearsDuration} years;`;
+  } else if (monthsDuration > 0) {
+    duration = `duration: ${monthsDuration} months`;
+  } else if (daysDuration > 0) {
+    duration = `duration: ${daysDuration} days`;
   } else {
-    duration = `duration: $ {dayDuration} days`;
+    return alert("invalid date input!");
   }
   console.log(image);
 
@@ -149,51 +151,3 @@ function getFullTime(time) {
 
   return `${date} ${monthName[monthIndex]} ${year} ${hours}:${minutes} WIB`;
 }
-
-// function getDistanceTime(time) {
-//   let timeNow = new Date();
-//   let timePost = time;
-
-//   // waktu sekarang - waktu post
-//   let distance = timeNow - timePost; // hasilnya milidetik
-//   console.log(distance);
-
-//   let milisecond = 1000; // milisecond
-//   let secondInHours = 3600; // 1 jam 3600 detik
-//   let hoursInDays = 24; // 1 hari 24 jam
-//   let daysInWeek = 7; // 1 minggu 7 hari
-//   let weeksInMonth = 4; // 1 bulan 4 minggu
-//   let monthsInYear = 12; // 1 tahun 12 bulan
-
-//   let distanceDay = Math.floor(
-//     distance / (milisecond * secondInHours * hoursInDays)
-//   );
-//   let distanceWeek = Math.floor(
-//     distance / (milisecond * secondInHours * hoursInDays * daysInWeek)
-//   );
-//   let distanceMonth = Math.floor(
-//     distance /
-//       (milisecond * secondInHours * hoursInDays * daysInWeek * weeksInMonth)
-//   );
-//   let distanceYear = Math.floor(
-//     distance /
-//       (milisecond *
-//         secondInHours *
-//         hoursInDays *
-//         daysInWeek *
-//         weeksInMonth *
-//         monthsInYear)
-//   );
-
-//   if (distanceYear > 0) {
-//     return `duration: ${distanceYear} years`;
-//   } else if (distanceMonth > 0) {
-//     return `duration: ${distanceMonth} months`;
-//   } else if (distanceWeek > 0) {
-//     return `duration: ${distanceWeek} weeks`;
-//   } else if (distanceDay > 0) {
-//     return `duration: ${distanceDay} days`;
-//   } else {
-//     return alert("Invalid Date input");
-//   }
-// }
