9fb68f6f86fb84bc5e9f18a0bccf7c307da22608
malfazakki <malfazakki@gmail.com>
2023-06-06 20:15:55 +0700

    Update Golang

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
 )
-
+ 
 func main() {
 	e := echo.New()
 
@@ -17,6 +18,7 @@ func main() {
 	e.GET("/add-project", addProject)
 	e.GET("/testimonial", testimonial)
 	e.GET("/project-detail", projectDetail)
+	e.POST("/add-project", formAddProject)
 	e.Logger.Fatal(e.Start("localhost:5000"))
 }
 
@@ -64,11 +66,10 @@ func projectDetail(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 
 	data := map[string]interface{}{
-		"Id": id,
-		"Title": "Title",
-		"Content": "Content",
+		"id":      id,
+		"Title":  "Dumbways Web App",
+		"Content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nemo repudiandae voluptas voluptatibus modi inventore totam quaerat itaque fugiat labore! Aliquid cumque nulla iusto eaque sequi impedit rerum harum magni minus vel? Officiis quod magnam minus asperiores repellendus, autem nemo quaerat aliquid, porro nesciunt ex mollitia. Veritatis architecto voluptatem earum amet dolor enim molestias, dicta qui magni similique vero! Quis obcaecati voluptas non eum amet, mollitia, ut commodi explicabo ad praesentium debitis nemo dicta voluptatum! Voluptatum odit a voluptas, quidem temporibus inventore! Iste repellat vitae autem! Ullam expedita atque odio dolorem laudantium tempora adipisci autem nulla iste at sequi eum eaque vero blanditiis, quis tempore molestias fugiat inventore exercitationem.",
 	}
-
 	var tmpl, err = template.ParseFiles("views/project-detail.html")
 
 	if err != nil {
@@ -76,4 +77,20 @@ func projectDetail(c echo.Context) error {
 	}
 
 	return tmpl.Execute(c.Response(), data)
+}
+
+func formAddProject(c echo.Context) error {
+	title := c.FormValue("input-project-name")
+	startDate := c.FormValue("input-date-start")
+	endDate := c.FormValue("input-date-end")
+	content := c.FormValue("input-deskripsi")
+
+	fmt.Println("Title :", title)
+	fmt.Println("Start Date :", startDate)
+	fmt.Println("End Date :", endDate)
+	fmt.Println("Content :", content)
+
+	
+
+	return c.Redirect(http.StatusMovedPermanently, "/project-detail")
 }
